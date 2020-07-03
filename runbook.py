1a15e3c71a451a1866015a86c3d4046c52a0581d
Aramide Oluwatosin <aoluwatosin10@gmail.com>
2020-07-03 23:18:00 +0000

    Update

--- runbook.py
+++ runbook.py
@@ -1,14 +1,15 @@
 from nornir import InitNornir
-from nornir.plugins.tasks.networking import netmiko_send_command,netmiko_send_config
+from nornir.plugins.tasks.networking import netmiko_send_command, netmiko_send_config
 from nornir.plugins.functions.text import print_title, print_result
 
 nr = InitNornir(config_file="config.yaml")
+
 
 def cdp_map(task):
     r = task.run(task=netmiko_send_command, command_string="show cdp neighbor", use_genie=True)
     task.host["facts"] = r.result
     outer = task.host["facts"]
-    indexer =outer["cdp"]['index']
+    indexer = outer["cdp"]['index']
     for idx in indexer:
         local_intf = indexer[idx]['local_interface']
         remote_port = indexer[idx]['port_id']
@@ -16,8 +17,9 @@ def cdp_map(task):
         if local_intf != 'GigabitEthernet0/0':
             cdp_config = task.run(netmiko_send_config, name='Automating CDP Network Description',
                                   config_commands=[
-                                      'interface '+str(local_intf),
-                                      'description ' + str(task.host)+'.cisco'+ ' ' +str(local_intf) +' <> '+ str(remote_id) + ' ' + str(remote_port)
+                                      'interface ' + str(local_intf),
+                                      'description ' + str(task.host) + '.cisco' + ' ' + str(local_intf) + ' <> ' + str(
+                                          remote_id) + ' ' + str(remote_port)
                                   ])
 
 
@@ -25,6 +27,7 @@ def main():
     # nr.data.dry_run = True
     result = nr.run(task=cdp_map)
     print_result(result)
+
 
 if __name__ == '__main__':
     main()
